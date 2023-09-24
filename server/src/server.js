6aa40a74ffaa60895a17b936b9d3c3fcc587969f
Ian <goldfeld35@gmail.com>
2023-09-24 00:01:05 -0300

    Dealing with Errors ❌

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,24 @@
+require('express-async-errors');
+const AppError = require('./utils/AppError');
+
 const express = require('express');
 
 const app = express();
+
 app.use(express.json());
 
-app.get('/', (request, reply) => {
-    reply.send('<h1>Hello World!</h1>');
+app.use((error, request, reply, next) => {
+    if (error instanceof AppError) {
+        return reply.status(statusCode).json({
+            status: 'error',
+            message: error.message
+        });
+    }
+
+    return reply.status(500).json({
+        status: 'error',
+        message: error.message
+    })
 });
 
 app.listen(3333, () => {
