3964c45ab5c1f280afbaa49ab4797bf9e77ace9f
Ian <goldfeld35@gmail.com>
2023-10-12 21:40:00 -0300

    Adding Cors 🎆

--- server/src/server.js
+++ server/src/server.js
@@ -5,10 +5,13 @@ const express = require('express');
 const AppError = require('./utils/AppError');
 const router = require('./routes');
 
+const cors = require('cors');
+
 const uploadConfig = require('./configs/upload');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
