ea11b8dbd0832e4425328bc6fd7d092db55ec295
Ian <goldfeld35@gmail.com>
2023-10-15 21:37:59 -0300

    Sign In 🎄

--- web/src/pages/SignIn/index.jsx
+++ web/src/pages/SignIn/index.jsx
@@ -2,10 +2,22 @@ import { Container, Main, Background, TextWrapper, Form, InputWrapper, Button } 
 
 import { Link } from 'react-router-dom';
 
+import { useState } from 'react';
+import { useAuth } from '../../hooks/auth';
+
 import { Input } from '../../components/Input';
 import { FiMail, FiLock } from 'react-icons/fi';
 
 export function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const { signIn } = useAuth();
+
+    function handleSignIn() {
+        signIn({ email, password })
+    }
+
     return (
         <Container>
             <Main>
@@ -14,15 +26,15 @@ export function SignIn() {
                     <p>Aplicação para acompanhar tudo que assistir.</p>
                 </TextWrapper>
 
-                <Form>
+                <Form onSubmit={e => e.preventDefault()}>
                     <h2>Faça seu login</h2>
 
                     <InputWrapper>
-                        <Input icon={FiMail} title={'E-mail'}/>
-                        <Input icon={FiLock} title={'Senha'}/>
+                        <Input icon={FiMail} type='email' title={'E-mail'} onChange={e => setEmail(e.target.value)}/>
+                        <Input icon={FiLock} type='password' title={'Senha'} onChange={e => setPassword(e.target.value)}/>
                     </InputWrapper>
 
-                    <Button>
+                    <Button onClick={handleSignIn}>
                         Entrar
                     </Button>
                 </Form>
--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
 
+import { useAuth } from '../hooks/auth';
+
 import { AppRoutes } from './app.routes';
 import { AuthenticationRoutes } from './authentication.routes';
 
 export function Routes() {
+    const { user } = useAuth();
+
     return (
         <BrowserRouter>
-            <AuthenticationRoutes />
+            { user ? <AppRoutes /> : <AuthenticationRoutes /> }
         </BrowserRouter>
     )
 }
